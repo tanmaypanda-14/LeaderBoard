c4084fc2b4a0ae56b3303e9317c452cdc09a09bd
Tanmay Panda <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-08-31 17:27:35 +0000

    22:57

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  return (
-    <h1>Hello</h1>
-  );
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home"
+import Admin from "./pages/Admin/Admin"
 
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 export default App;
